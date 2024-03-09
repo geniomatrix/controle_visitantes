b72dc90c90b78fcda6ecc9db156fe73eaf349a19
Eugenio Arcanjo <52720803+geniomatrix@users.noreply.github.com>
2024-03-09 15:18:49 -0300

    criado o botão de buscar na area admin para socio e dependentes

--- apps/socio/admin.py
+++ apps/socio/admin.py
@@ -2,5 +2,12 @@ from django.contrib import admin
 
 from .models import Socio, Dependentes
 
-admin.site.register(Socio)
-admin.site.register(Dependentes)
+class SocioAdmin(admin.ModelAdmin):
+    search_fields = ['nome', 'email', 'tpsocio']  # Lista de campos pelos quais deseja realizar a busca
+
+class DependAdmin(admin.ModelAdmin):
+    search_fields = ['nome', 'tpsocio']  # Lista de campos pelos quais deseja realizar a busca
+
+admin.site.register(Socio,SocioAdmin)
+admin.site.register(Dependentes,DependAdmin)
+
